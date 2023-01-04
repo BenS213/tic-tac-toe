1d9bd0d2e2843ebfa8758499eab051c4b953f25f
BenS213 <57325318+BenS213@users.noreply.github.com>
2023-01-03 21:34:43 -0500

    add tie functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function App() {
+  const initialGrid = ["", "", "", "", "", "", "", "", ""];
+  const [grid, setGrid] = useState(initialGrid);
+  const [XTurn, setXTurn] = useState(true);
+  const [XMoves, setXMoves] = useState([]);
+  const [OMoves, setOMoves] = useState([]);
+  const [winner, setWinner] = useState("");
+  const [plays, setPlays] = useState(0);
+
+  function handleClick(i) {
+    setPlays(plays => plays + 1)
+    if (grid[i] === "X" || grid[i] === "O") {
+      return console.log("already taken");
+    }
+    grid[i] = XTurn ? <FontAwesomeIcon icon={solid('x')} /> : <FontAwesomeIcon icon={solid('o')} />;
+    setXTurn(XTurn ? false : true);
+    XTurn ? setXMoves([...XMoves, i + 1]) : setOMoves([...OMoves, i + 1]);
+    setGrid([...grid]);
+    if (plays > 7) setWinner('Tie')
+  }
+
+  useEffect(() => {
+      const winningCombos = [
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+      ];
+      
+        for (const combination of winningCombos) {
+          const [a, b, c] = combination;
+          if (XMoves.includes(a) && XMoves.includes(b) && XMoves.includes(c)) {
+            return setWinner("X");
+          }
+          if (OMoves.includes(a) && OMoves.includes(b) && OMoves.includes(c)) {
+            return setWinner("O");
+          }
+        }
+    }, [XMoves, OMoves]
+
+  )
+
+  function reset() {
+    setGrid(initialGrid);
+    setXMoves([]);
+    setOMoves([]);
+    setWinner('');
+    setXTurn(true);
+    setPlays(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>TIC-TAC-TOE</h1>
       </header>
-    </div>
+      <div className="scoreBoard">
+        {!winner ? `Turn: ${XTurn ? "X" : "O"}` : `winner: ${winner}`}
+      </div>
+      <div className={!winner ? "board" : "board gameOver"}>
+        {grid.map((square, i) => (
+          <div key={i} onClick={() => handleClick(i)} className="square">
+            {square}
+          </div>
+        ))}
+      </div>
+      <div className="buttons">
+        {winner ? <button className='resetButton' onClick={reset}>Reset</button> : ''}
+      </div>
+    </>
   );
 }
 
